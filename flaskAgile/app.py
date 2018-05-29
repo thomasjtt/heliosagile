a92a76c6628f630c9efa9ccf9ac01da9b38d0369
Jobin Thomas <jobin@superiordatascience.com>
2018-05-28 19:40:03 -0500

    mongo

--- flaskAgile/app.py
+++ flaskAgile/app.py
@@ -2,6 +2,11 @@ import json
 
 from flask import Flask, render_template
 import pandas as pd
+from pymongo import MongoClient
+
+client = MongoClient("localhost", 27017)
+db = client.agile
+
 
 app = Flask(__name__)
 
@@ -11,6 +16,8 @@ def index():
 	chart_data = df.to_dict(orient='records')
 	chart_data = json.dumps(chart_data, indent=2)
 	data = {'chart_data': chart_data}
+
+	print data
 	return render_template("index.html", data=data)
 
 @app.route("/login")
@@ -23,10 +30,24 @@ def login():
 
 @app.route("/d3plot")
 def d3plot():
-	df = pd.read_csv('data.csv').drop('Open', axis=1)
-	chart_data = df.to_dict(orient='records')
+
+	collection = db['d3example']
+	cursor = collection.find({})
+	chart_data = []
+	for document in cursor:
+		# document.pop['_id']
+		document = {'Date':document['Date'],'High':float(document['High']), 'Low':float(document['Low']),'Close':float(document['Close']) }
+		chart_data.append(document)
+
 	chart_data = json.dumps(chart_data, indent=2)
 	data = {'chart_data': chart_data}
+
+	print data
+
+	# df = pd.read_csv('data.csv').drop('Open', axis=1)
+	# chart_data = df.to_dict(orient='records')
+	# chart_data = json.dumps(chart_data, indent=2)
+	# data = {'chart_data': chart_data}
 	return render_template("d3plotExample.html", data=data)
 
 if __name__ == "__main__":
